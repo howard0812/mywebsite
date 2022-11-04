0a22a17cc47ec9664cf78c9fea5c1fa55838c196
howard0812 <keepgoing876@gmail.com>
2022-11-05 05:27:15 +0800

    第一次修改

---
+++ main.js
@@ -0,0 +1,8 @@
+// 選到選到 .navbar-toggler 並綁定點擊事件
+$(".navbar-toggler").click(function(){
+    // 當這顆按鈕被點後要做的事情
+    // alert("按鈕被點了");
+    // 切換 .navbar-list 的 active class
+    $(".navbar-list").toggleClass("active");
+
+} ) ;
